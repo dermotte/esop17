3e2ac14c3e6863a776ec4f76db96c56e8a3d4113
Mathias Lux <mathias.lux@aau.at>
2017-10-05 11:20:54 +0200

    added lesson02 and lesson03

---
+++ src/main/java/at/aau/itec/esop17/lesson02/SimpleProgram.java
@@ -0,0 +1,12 @@
+package at.aau.itec.esop17.lesson02;
+
+/**
+ * Created by mlux on 05.10.2017.
+ */
+public class SimpleProgram {
+    public static void main(String[] args) {
+        int x = 1;
+        x++;
+        System.out.println(x);
+    }
+}
---
+++ src/main/java/at/aau/itec/esop17/lesson03/SimpleLoop.java
@@ -0,0 +1,13 @@
+package at.aau.itec.esop17.lesson03;
+
+/**
+ * Created by mlux on 05.10.2017.
+ */
+public class SimpleLoop {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x++ < 10) {
+            System.out.println(x);
+        }
+    }
+}
